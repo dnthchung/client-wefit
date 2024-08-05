23ed92d99dd34c07b0616aba4975bbdbc002f8ee
dnthchung <doanthanhchung03nb@gmail.com>
2024-08-06 02:04:58 +0700

    add docker

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,22 +1,19 @@
-// import authorizedAxiosInstance from "~/utils/authorizedAxios";
+import authorizedAxiosInstance from "~/utils/authorizedAxios";
 import { API_ROOT } from "~/constants";
 
-// export const handleLogoutAPI = async () => {
-//   //Cách 1 : header - dùng localStorage - chỉ cần xóa thông tin user trong localStorage
-//   //nếu dùng cookie thì không cần 3 dòng này nữa
-//   localStorage.removeItem("userInfo");
-//   localStorage.removeItem("accessToken");
-//   localStorage.removeItem("refreshToken");
+export const handleLogoutAPI = async () => {
+  //Cách 1 : header - dùng localStorage - chỉ cần xóa thông tin user trong localStorage
+  //nếu dùng cookie thì không cần 3 dòng này nữa
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
 
-//   //Cách 2:  cookie - dùng http only cookie - cần gửi request lên server để xóa cookie trong headers của BE
-//   return await authorizedAxiosInstance.delete(`${API_ROOT}/v1/users/logout`);
-// };
+  //Cách 2:  cookie - dùng http only cookie - cần gửi request lên server để xóa cookie trong headers của BE
+  return await authorizedAxiosInstance.delete(`${API_ROOT}/v1/users/logout`);
+};
 
-// export const handleCallRefreshTokenAPI = async (refreshToken) => {
-//   return await authorizedAxiosInstance.put(
-//     `${API_ROOT}/v1/users/refresh_token`,
-//     {
-//       refreshToken,
-//     }
-//   );
-// };
+export const handleCallRefreshTokenAPI = async (refreshToken: any) => {
+  return await authorizedAxiosInstance.put(`${API_ROOT}/v1/users/refresh_token`, {
+    refreshToken,
+  });
+};
--- src/utils/authorizedAxios.ts
+++ src/utils/authorizedAxios.ts
@@ -1,1 +1,81 @@
+// Author: TrungQuanDev: https://youtube.com/@trungquandev
 import axios from "axios";
+import { toast } from "react-toastify";
+import { handleLogoutAPI, handleCallRefreshTokenAPI } from "~/apis";
+
+//khởi tạo 1 đối tượng axios (authorizedAxiosInstance) mục đích để custom và cấu hình chung cho dự án
+let authorizedAxiosInstance = axios.create();
+
+//thời gian chờ tối đa của 1 request : 10p
+authorizedAxiosInstance.defaults.timeout = 100 * 60 * 10;
+
+//phục vụ cho trường hợp chúng ta sử dụng JWT token (refresh và access) theo cơ chế httpOnly cookie
+authorizedAxiosInstance.defaults.withCredentials = true;
+
+authorizedAxiosInstance.interceptors.request.use(
+  (config) => {
+    // Lấy access token từ localStorage và đính kèm vào header của mỗi request
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+let refreshTokenPromise: any = null;
+authorizedAxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    /**======================== xử lý auto refresh token =================== */
+    if (error.response?.status === 401) {
+      handleLogoutAPI().then(() => {
+        //nếu mà dùng cookie thì cần xóa user info trong localStorage - mở comment dòng dưới
+        // localStorage.removeItem("userInfo");
+        location.href = "/login";
+      });
+    }
+    const originalRequest = error.config;
+    console.log("originalRequest", originalRequest);
+    if (error.response?.status === 410 && originalRequest) {
+      originalRequest._retry = true;
+
+      if (!refreshTokenPromise) {
+        //lấy refresh token từ localStorage
+        const refreshToken = localStorage.getItem("refreshToken");
+        //call api refresh token
+        refreshTokenPromise = handleCallRefreshTokenAPI(refreshToken)
+          .then((res) => {
+            const { accessToken } = res.data;
+            localStorage.setItem("accessToken", accessToken);
+            authorizedAxiosInstance.defaults.headers.authorization = `Bearer ${accessToken}`;
+          })
+          .catch((_error) => {
+            handleLogoutAPI().then(() => {
+              location.href = "/login";
+            });
+            return Promise.reject(_error);
+          })
+          .finally(() => {
+            refreshTokenPromise = null;
+          });
+      }
+
+      return refreshTokenPromise.then(() => {
+        return authorizedAxiosInstance(originalRequest);
+      });
+    }
+
+    if (error.response?.status !== 410) {
+      toast.error(error.response?.data?.message || error?.message);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default authorizedAxiosInstance;
